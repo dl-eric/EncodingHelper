d953bcc7145488892366bb5726eb837869b4846e
dl-eric <orangepamelo@gmail.com>
2015-09-19 21:19:27 -0400

    Add Initial Code

---
+++ edu/andover/elee/EncodingHelperCharTest.java
@@ -0,0 +1,54 @@
+package edu.andover.elee;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class EncodingHelperCharTest {
+
+	@Test
+	public void testEncodingHelperCharInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEncodingHelperCharByteArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEncodingHelperCharChar() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCodepoint() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetCodepoint() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToUtf8Bytes() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToCodepointString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToUtf8String() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCharacterName() {
+		fail("Not yet implemented");
+	}
+
+}
